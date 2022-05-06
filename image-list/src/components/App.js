b65f36ecf397bde6f6f2f25ab58d30e4dca2c98d
ErdalErden <you@example.com>
2022-05-07 00:30:54 +0300

    image-list app çalışıldı

---
+++ image-list/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Searchinput from './Searchinput'
+
+const App = () => {
+  return (
+    <div className='ui container' style={{marginTop:'30px'}}>
+      <Searchinput/>
+    </div>
+  )
+}
+
+export default App
