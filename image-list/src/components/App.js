0c310e850e9e25042d5f32836e6caff8fe4cd522
Erdal Erden <57586690+ErdalErden@users.noreply.github.com>
2022-05-18 17:28:24 +0300

    apiden veri alımı tamamlandı

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 import SearchInput from './SearchInput'
 
 class App extends React.Component{
 
-  onSearchSubmit = (entry) => {
-    console.log(entry);
+  async onSearchSubmit(entry){
+    const response = await axios.get(`https://pixabay.com/api/?key=27493808-be1187df3aed352a9abea61e2&q=${entry}&image_type=photo`)
+    console.log(response.data.hits);
   }
   render(){
       return (
